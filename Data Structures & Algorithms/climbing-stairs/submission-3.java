055d3553e13a01abf979b1dc538e81e425c865e3
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2026-08-31 23:46:37 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    private static int[] dp = new int[46];
+
+    static {
+        Arrays.fill(dp, -1);
+    }
+
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n == 2) {
+            return 2;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+
+        return dp[n];
+    }
+}
